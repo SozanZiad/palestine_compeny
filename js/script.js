e8ba909f02fbc5a44528b8304748f64137204f20
SozanZiad <92858341+SozanZiad@users.noreply.github.com>
2023-03-14 14:58:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var btn = document.querySelector('.search');
+var menu = document.querySelector('.search-div');
+
+
+btn.addEventListener('click', function () {
+    menu.style.display =
+        menu.style.display === 'none' ? 'block' : 'none';
+})
+
+var dn = document.querySelector('.dn');
+var dropdown = document.querySelector('.dropdown-menu');
+
+
+dn.addEventListener('click', function () {
+    dropdown.style.display =
+        dropdown.style.display === 'none' ? 'block' : 'none';
+})
+
+var add= document.querySelector('.dn').onclick =() =>
+document.querySelector('.oevrllay').style.display='block'
+
+var add= document.querySelector('.cancl img').onclick =() =>
+document.querySelector('.oevrllay').style.display='none'
+
+
+var add= document.querySelector('.dn2').onclick =() =>
+document.querySelector('.oevrllay2').style.display='block'
+
+var add= document.querySelector('.cancl2 img').onclick =() =>
+document.querySelector('.oevrllay2').style.display='none'
+
+var add= document.querySelector('.dn3').onclick =() =>
+document.querySelector('.oevrllay3').style.display='block'
+
+var add= document.querySelector('.cancl3 img').onclick =() =>
+document.querySelector('.oevrllay3').style.display='none'
+
+var add= document.querySelector('.dn4').onclick =() =>
+document.querySelector('.oevrllay4').style.display='block'
+
+var add= document.querySelector('.cancl4 img').onclick =() =>
+document.querySelector('.oevrllay4').style.display='none'
